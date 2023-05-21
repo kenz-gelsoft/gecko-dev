ea955c584aa1b24c54c5d2bd3dcfa1e9c5eff74e
KENZ <KENZ.gelsoft@gmail.com>
2023-05-21 12:27:12 +0900

    Fix compilation error on GCC
    * You can specify to use GCC by setting enrironment variables like `CC=gcc`, `CXX=g++`.
    * (Current Mozilla default) Clang detects HaikuOS as some UNIX-like variant, but GCC doesn't.
    * Teach some libraries Haiku OS is UNIX-like.

--- gfx/skia/skia/include/core/SkPreConfig.h
+++ gfx/skia/skia/include/core/SkPreConfig.h
@@ -31,7 +31,7 @@
         #define SK_BUILD_FOR_ANDROID
     #elif defined(linux) || defined(__linux) || defined(__FreeBSD__) || \
           defined(__OpenBSD__) || defined(__sun) || defined(__NetBSD__) || \
-          defined(__DragonFly__) || defined(__Fuchsia__) || \
+          defined(__DragonFly__) || defined(__Fuchsia__) || defined(__HAIKU__) || \
           defined(__GLIBC__) || defined(__GNU__) || defined(__unix__)
         #define SK_BUILD_FOR_UNIX
     #elif TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
